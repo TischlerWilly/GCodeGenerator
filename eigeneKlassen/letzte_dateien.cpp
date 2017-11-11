baf2efed47415b32a5a7fd9e4888b23056367936
Oliver Schuft <oliver.schuft@gmx.de>
2017-11-11 23:02:30 +0100

    wieder Öffnen der letzten offenen Dateien 2/2

---
+++ eigeneKlassen/letzte_dateien.cpp
@@ -0,0 +1,70 @@
+#include "letzte_dateien.h"
+
+letzte_dateien::letzte_dateien()
+{
+    set_anz_eintreage(3);
+}
+
+letzte_dateien::letzte_dateien(uint anzahl_der_eintraege)
+{
+    set_anz_eintreage(anzahl_der_eintraege);
+}
+
+void letzte_dateien::set_anz_eintreage(uint neue_anz)
+{
+    //darf nicht verwendet werde, wenn bereits Daten eingelesen sind
+    if(neue_anz > 1)
+    {
+        dateinamen.clear();
+        anz_eintraege = neue_anz;
+    }
+}
+
+void letzte_dateien::datei_merken(QString name)
+{
+    QString tmp = dateinamen.get_text();
+    if(tmp.contains(name))
+    {
+        uint pos=0;
+        for(uint i = 1 ; i<=dateinamen.zeilenanzahl() ; i++)
+        {
+            if(dateinamen.zeile(i) == name)
+            {
+                pos = i;
+                break;
+            }
+        }
+        dateinamen.zeilen_loeschen(pos,1);
+        dateinamen.zeile_anhaengen(name);
+    }else if(dateinamen.zeilenanzahl() < anz_eintraege)
+    {
+        dateinamen.zeile_anhaengen(name);
+    }else
+    {
+        dateinamen.zeilen_loeschen(1,1);
+        dateinamen.zeile_anhaengen(name);
+    }
+}
+
+QString letzte_dateien::get_text()
+{
+    QString retstr;
+    //Reihenfolge der Einträge umdrehen:
+    for(uint i=dateinamen.zeilenanzahl(); i>0;i--)
+    {
+        retstr += dateinamen.zeile(i);
+        retstr += "\n";
+    }
+    retstr = retstr.left(retstr.length() - 1);//letzets Zeichen löschen = "\n"
+    return retstr;
+}
+void letzte_dateien::set_text(QString liste)
+{
+    text_zeilenweise tz;
+    tz.set_text(liste);
+
+    if(tz.zeilenanzahl() <= anz_eintraege)
+    {
+        dateinamen = tz;
+    }
+}
---
+++ eigeneKlassen/letzte_dateien.h
@@ -0,0 +1,32 @@
+#ifndef LETZTE_DATEIEN_H
+#define LETZTE_DATEIEN_H
+
+#include "eigeneKlassen/text_zeilenweise.h"
+
+class letzte_dateien
+{
+public:
+    letzte_dateien();
+    letzte_dateien(uint anzahl_der_eintraege);
+
+    QString get_text();
+    inline uint get_anz_eintreage()
+    {
+        return anz_eintraege;
+    }
+
+    void set_text(QString liste);
+    void set_anz_eintreage(uint neue_anz);
+    void datei_merken(QString name);
+
+
+
+private:
+    text_zeilenweise dateinamen;
+    uint anz_eintraege;
+
+
+
+};
+
+#endif // LETZTE_DATEIEN_H
