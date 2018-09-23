a703ab583a8fd71ee2ae529e57b6b49a518d581c
TischlerWilly <oliver.schuft@gmx.de>
2018-09-23 14:02:13 +0200

    E-Zahlen Bugfix

--- eigeneKlassen/rechtecktasche.cpp
+++ eigeneKlassen/rechtecktasche.cpp
@@ -388,7 +388,8 @@ string rechtecktasche::get_gcode_ausraumen(rechteck r, float radius, float tiefe
     return gcode;
 }
 
-string rechtecktasche::get_rechteckbahn_UZS(rechteck r, float radius, float tiefe_vor_eintauchen, float tiefe_nach_eintauchen)
+string rechtecktasche::get_rechteckbahn_UZS(rechteck r, float radius, \
+                                            float tiefe_vor_eintauchen, float tiefe_nach_eintauchen)
 {
     //Das an die Funktioen übergebene Rechteck beschreibt die größe der Fräsbahn auf Mitte des Fräsers
     string gcode;
